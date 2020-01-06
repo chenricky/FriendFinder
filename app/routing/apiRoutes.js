65113af50fd665f70553cafc14079f1a02a3eeb9
Ricky Chen <chenricky@gmail.com>
2020-01-06 13:06:02 -0800

    post is now working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -33,4 +33,17 @@ app.get("/", function(req, res) {
   
     return res.json(false);
   });
+
+// post
+
+app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    
+      freindsData.push(req.body);
+      res.json(true);
+    
+  });
+
 };
